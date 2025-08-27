48397e68b0251c018e305c0e5a55720f3805032a
Adilson-Kina <adilsonkinakina@gmail.com>
2025-08-27 14:32:43 -0300

    WIP: to refactor

--- 2-semestre/script/script.js
+++ 2-semestre/script/script.js
@@ -1,7 +1,7 @@
 function getDate() {
   let date = new Date();
   let day = date.getDate();
-  let month = date.getMonth();
+  let month = date.getMonth() + 1;
   let year = date.getFullYear();
   let dateArray = [day, month, year];
   let dateFormated = dateArray.join("/");
@@ -9,3 +9,53 @@ function getDate() {
 }
 
 window.onload = getDate;
+window.onload = change;
+
+function change(){
+  document.getElementById('register-container').style.backgroundColor = getCookie('bg');
+  document.getElementById('register-container').style.color = getCookie('font');
+  document.getElementById('index-container').style.backgroundColor = getCookie('bg');
+  document.getElementById('index-container').style.color = getCookie('font');
+}
+
+function changeBg(){
+  getColor();
+  document.getElementById('index-container').style.backgroundColor = getCookie('bg');
+  document.getElementById('register-container').style.backgroundColor = getCookie('bg');
+}
+
+function changeTxt(){
+  getColor();
+  document.getElementById('index-container').style.color = getCookie('font');
+  document.getElementById('register-container').style.color = getCookie('font');
+}
+
+function getColor(){
+  let bgColor = document.getElementById('bgColor').value;
+  let fontColor = document.getElementById('fontColor').value;
+  setCookie('bg', bgColor);
+  setCookie('font', fontColor);
+}
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+} 
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
