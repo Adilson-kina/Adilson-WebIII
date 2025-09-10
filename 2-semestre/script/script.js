f544426841c6a6c60c86e15954e4a481bdedc6f4
Adilson-Kina <adilsonkinakina@gmail.com>
2025-09-10 17:57:07 -0300

    WIP: Frontend

--- 2-semestre/script/script.js
+++ 2-semestre/script/script.js
@@ -1,5 +1,19 @@
 let list =  ['./cadastro.html', './relatorio.html', './alteracao.html'];
+let date;
+
+function start(){
+        date = dateNow();
+        document.getElementById('date').innerHTML = date;
+}
+
+window.onload = start;
 
 function redirect(page){
         window.location.href = list[page];
 }
+
+function dateNow(){
+        let date = new Date();
+        return date.toLocaleDateString("en-GB");
+}
+
